1ac254a26f417a66cf15d943558185c8c98d8503
jaayydev <jbrennandev@gmail.com>
2021-11-11 23:12:40 +0000

    A function to repeat a string for x amount of times, codewars level 8 kata

--- string_repeat.py
+++ string_repeat.py
@@ -1,4 +1,7 @@
 # Write a function called repeatStr which repeats the given string string exactly n times.
 
-def repeatStr():
-    print("test")
+def repeat_str(repeat, string):
+    res = ""
+    for i in range(0, repeat):
+        res += string
+    return res
